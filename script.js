0ba1fc1cc784b246bda6d5b6695ba46bc9c477e3
mendes <rahelisonhajaina@gmail.com>
2024-01-31 09:23:13 +0200

    fix showing file-name

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let inputFile = document.getElementById('file-ul');
 let testInputFile = document.getElementById('file-lg');
 let inputContainer = inputFile.parentElement;
 
-
+// test upload file
 inputFile.addEventListener('change', function() {
     if (inputFile.disabled) {
         inputContainer.classList.add('disabled');
@@ -11,25 +11,40 @@ inputFile.addEventListener('change', function() {
         inputContainer.classList.remove('disabled');
     }
 });
+// for the file name on the input
 
+var ro = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      const fileNameDiv = entry.target.querySelector('.file-name');
+      const width = entry.contentRect.width;
+      if (fileNameDiv) fileNameDiv.style.maxWidth = ( width -50) + 'px';
+    }
+  });
+  
+let containers = document.querySelectorAll('.input-file-container');
+  
 //listen input file change
 
 function listenInputFileChange(element) {
     element.addEventListener('change', function(e) {
-        var fileName = e.target.files[0].name;
-        var parentContainer = e.target.parentElement;
-        var label = parentContainer.querySelector('.input-label');
-        var fileNameDiv = parentContainer.querySelector('.file-name');
+        let fileName = e.target.files[0].name;
+        let parentContainer = e.target.parentElement;
+        let label = parentContainer.querySelector('.input-label');
+        let fileNameDiv = document.createElement('div');
+        fileNameDiv.className = 'file-name';
+        fileNameDiv.textContent = fileName;
     
         if (parentContainer.classList.contains('input-container-underline')) {
             label.style.transform = 'translateY(-125%)'; 
-            fileNameDiv.textContent = fileName;
         } else {
-            label.textContent = fileName;
+            label.style.display = 'none'
         }
+        parentContainer.appendChild(fileNameDiv);
+        containers.forEach(container => ro.observe(container));
     });
     
 }
+
 
 listenInputFileChange(testInputFile);
 listenInputFileChange(inputFile);
